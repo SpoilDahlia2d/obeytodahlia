a3d42befd03f109e4b9c138dffe1827f4e179c53
SpoilDahlia2d <dahliastar1998@gmail.com>
2025-07-31 21:50:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
- from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory
 import os
 import random
 
@@ -9,15 +9,16 @@ def index():
     return render_template('index.html')
 
 @app.route('/images')
-def images():
-    folder = os.path.join(app.static_folder, 'images')
-    files = [f for f in os.listdir(folder) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.webp'))]
+def get_images():
+    image_folder = 'images'
+    valid_extensions = ('.png', '.jpg', '.jpeg', '.gif')
+    files = [f for f in os.listdir(image_folder) if f.lower().endswith(valid_extensions)]
     random.shuffle(files)
     return {'images': files}
 
-@app.route('/static/images/<filename>')
+@app.route('/images/<path:filename>')
 def serve_image(filename):
-    return send_from_directory(os.path.join(app.static_folder, 'images'), filename)
+    return send_from_directory('images', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
