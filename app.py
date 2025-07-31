dddc812baa843f65b51b305686b5802c3e2eb9cf
SpoilDahlia2d <dahliastar1998@gmail.com>
2025-07-31 21:09:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/images')
-def images():
-    img_folder = os.path.join(app.static_folder, 'images')
-    files = os.listdir(img_folder)
-    images = [f for f in files if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
+@app.route("/images")
+def get_images():
+    image_folder = os.path.join("static", "images")
+    extensions = (".png", ".jpg", ".jpeg", ".webp")
+    images = [f for f in os.listdir(image_folder) if f.lower().endswith(extensions)]
     return jsonify(images)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
