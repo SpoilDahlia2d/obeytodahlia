3e4d0a5d9c74b14337a921aff12075adf774472f
SpoilDahlia2d <dahliastar1998@gmail.com>
2025-08-01 21:14:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, jsonify
 import os
 import random
 
@@ -10,15 +10,13 @@ def index():
 
 @app.route('/images')
 def get_images():
-    image_folder = 'images'
-    valid_extensions = ('.png', '.jpg', '.jpeg', '.gif')
-    files = [f for f in os.listdir(image_folder) if f.lower().endswith(valid_extensions)]
-    random.shuffle(files)
-    return {'images': files}
-
-@app.route('/images/<path:filename>')
-def serve_image(filename):
-    return send_from_directory('images', filename)
+    image_dir = os.path.join(app.static_folder, 'images')
+    all_images = [
+        f'/static/images/{f}' for f in os.listdir(image_dir)
+        if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))
+    ]
+    random.shuffle(all_images)
+    return jsonify(all_images)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
